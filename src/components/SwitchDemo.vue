77c5203ada6a053bac968369164257ec933e3d17
bela <1981253197@qq.com>
2021-10-19 00:26:19 +0800

    完成switch组件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>SwitchDemo 页面</div>
+  <div><Switch v-model:value="bool" /></div>
 </template>
+
+<script lang="ts">
+import { ref } from "vue";
+import Switch from "../lib/Switch.vue";
+export default {
+  components: { Switch },
+  setup() {
+    const bool = ref(false);
+    return { bool };
+  },
+};
+</script>
