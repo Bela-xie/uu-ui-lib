3797ceb4e1274dfbdc6a6b8bbf4dd78169be004e
bela <1981253197@qq.com>
2021-10-21 11:48:50 +0800

    完成 tabs 组件

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>TabsDemo 页面</div>
+  <div>
+    <h2>tabs 示例一</h2>
+    <Tabs v-model:selected="selected">
+      <Tab title="标题一">tab1</Tab>
+      <Tab title="标题二3343">tab2</Tab>
+      <Tab title="标题三aaaaaaa">tab3</Tab>
+    </Tabs>
+  </div>
 </template>
+
+<script lang="ts">
+import Tabs from "../lib/Tabs.vue";
+import Tab from "../lib/Tab.vue";
+import { ref } from "vue";
+export default {
+  components: {
+    Tabs,
+    Tab,
+  },
+  setup() {
+    const selected = ref("标题一");
+    return { selected };
+  },
+};
+</script>
